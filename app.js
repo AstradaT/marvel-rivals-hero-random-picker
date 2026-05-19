4a944733a81e046132169ffe153387399a5b562a
AstradaT <tomi_cuervocasla@hotmail.com>
2026-05-18 21:19:51 -0300

    Changed spinning dynamic images with static ones

--- app.js
+++ app.js
@@ -10,62 +10,62 @@ let isMuted = false;
 // Hero data
 const heroes = [
     // --- DUELISTS ---
-    { name: "Black Cat", role: "Duelist", img: "assets/blackcat.webp" },
-    { name: "Black Panther", role: "Duelist", img: "assets/blackpanther.webp" },
-    { name: "Black Widow", role: "Duelist", img: "assets/blackwidow.webp" },
-    { name: "Blade", role: "Duelist", img: "assets/blade.webp" },
-    { name: "Daredevil", role: "Duelist", img: "assets/daredevil.webp" },
-    { name: "Deadpool", role: "Duelist", img: "assets/deadpool.webp" },
-    { name: "Elsa Bloodstone", role: "Duelist", img: "assets/elsabloodstone.webp" },
-    { name: "Hawkeye", role: "Duelist", img: "assets/hawkeye.webp" },
-    { name: "Hela", role: "Duelist", img: "assets/hela.webp" },
-    { name: "Human Torch", role: "Duelist", img: "assets/humantorch.webp" },
-    { name: "Iron Fist", role: "Duelist", img: "assets/ironfist.webp" },
-    { name: "Iron Man", role: "Duelist", img: "assets/ironman.webp" },
-    { name: "Magik", role: "Duelist", img: "assets/magik.webp" },
-    { name: "Mister Fantastic", role: "Duelist", img: "assets/misterfantastic.webp" },
-    { name: "Moon Knight", role: "Duelist", img: "assets/moonknight.webp" },
-    { name: "Namor", role: "Duelist", img: "assets/namor.webp" },
-    { name: "Phoenix", role: "Duelist", img: "assets/phoenix.webp" },
-    { name: "Psylocke", role: "Duelist", img: "assets/psylocke.webp" },
-    { name: "Scarlet Witch", role: "Duelist", img: "assets/scarletwitch.webp" },
-    { name: "Spider-Man", role: "Duelist", img: "assets/spiderman.webp" },
-    { name: "Squirrel Girl", role: "Duelist", img: "assets/squirrelgirl.webp" },
-    { name: "Star-Lord", role: "Duelist", img: "assets/starlord.webp" },
-    { name: "Storm", role: "Duelist", img: "assets/storm.webp" },
-    { name: "The Punisher", role: "Duelist", img: "assets/thepunisher.webp" },
-    { name: "Winter Soldier", role: "Duelist", img: "assets/wintersoldier.webp" },
-    { name: "Wolverine", role: "Duelist", img: "assets/wolverine.webp" },
+    { name: "Black Cat", role: "Duelist", img: "assets/blackcat.webp", staticImg: "assets/static/blackcat.jpg" },
+    { name: "Black Panther", role: "Duelist", img: "assets/blackpanther.webp", staticImg: "assets/static/blackpanther.jpg" },
+    { name: "Black Widow", role: "Duelist", img: "assets/blackwidow.webp", staticImg: "assets/static/blackwidow.jpg" },
+    { name: "Blade", role: "Duelist", img: "assets/blade.webp", staticImg: "assets/static/blade.jpg" },
+    { name: "Daredevil", role: "Duelist", img: "assets/daredevil.webp", staticImg: "assets/static/daredevil.jpg" },
+    { name: "Deadpool", role: "Duelist", img: "assets/deadpool.webp", staticImg: "assets/static/deadpool.jpg" },
+    { name: "Elsa Bloodstone", role: "Duelist", img: "assets/elsabloodstone.webp", staticImg: "assets/static/elsabloodstone.jpg" },
+    { name: "Hawkeye", role: "Duelist", img: "assets/hawkeye.webp", staticImg: "assets/static/hawkeye.jpg" },
+    { name: "Hela", role: "Duelist", img: "assets/hela.webp", staticImg: "assets/static/hela.jpg" },
+    { name: "Human Torch", role: "Duelist", img: "assets/humantorch.webp", staticImg: "assets/static/humantorch.jpg" },
+    { name: "Iron Fist", role: "Duelist", img: "assets/ironfist.webp", staticImg: "assets/static/ironfist.jpg" },
+    { name: "Iron Man", role: "Duelist", img: "assets/ironman.webp", staticImg: "assets/static/ironman.jpg" },
+    { name: "Magik", role: "Duelist", img: "assets/magik.webp", staticImg: "assets/static/magik.jpg" },
+    { name: "Mister Fantastic", role: "Duelist", img: "assets/misterfantastic.webp", staticImg: "assets/static/misterfantastic.jpg" },
+    { name: "Moon Knight", role: "Duelist", img: "assets/moonknight.webp", staticImg: "assets/static/moonknight.jpg" },
+    { name: "Namor", role: "Duelist", img: "assets/namor.webp", staticImg: "assets/static/namor.jpg" },
+    { name: "Phoenix", role: "Duelist", img: "assets/phoenix.webp", staticImg: "assets/static/phoenix.jpg" },
+    { name: "Psylocke", role: "Duelist", img: "assets/psylocke.webp", staticImg: "assets/static/psylocke.jpg" },
+    { name: "Scarlet Witch", role: "Duelist", img: "assets/scarletwitch.webp", staticImg: "assets/static/scarletwitch.jpg" },
+    { name: "Spider-Man", role: "Duelist", img: "assets/spiderman.webp", staticImg: "assets/static/spiderman.jpg" },
+    { name: "Squirrel Girl", role: "Duelist", img: "assets/squirrelgirl.webp", staticImg: "assets/static/squirrelgirl.jpg" },
+    { name: "Star-Lord", role: "Duelist", img: "assets/starlord.webp", staticImg: "assets/static/starlord.jpg" },
+    { name: "Storm", role: "Duelist", img: "assets/storm.webp", staticImg: "assets/static/storm.jpg" },
+    { name: "The Punisher", role: "Duelist", img: "assets/thepunisher.webp", staticImg: "assets/static/thepunisher.jpg" },
+    { name: "Winter Soldier", role: "Duelist", img: "assets/wintersoldier.webp", staticImg: "assets/static/wintersoldier.jpg" },
+    { name: "Wolverine", role: "Duelist", img: "assets/wolverine.webp", staticImg: "assets/static/wolverine.jpg" },
 
     // --- VANGUARDS ---
-    { name: "Angela", role: "Vanguard", img: "assets/angela.webp" },
-    { name: "Captain America", role: "Vanguard", img: "assets/captainamerica.webp" },
-    { name: "Deadpool", role: "Vanguard", img: "assets/deadpool.webp" },
-    { name: "Devil Dinosaur", role: "Vanguard", img: "assets/devildinosaur.webp" },
-    { name: "Doctor Strange", role: "Vanguard", img: "assets/doctorstrange.webp" },
-    { name: "Emma Frost", role: "Vanguard", img: "assets/emmafrost.webp" },
-    { name: "Groot", role: "Vanguard", img: "assets/groot.webp" },
-    { name: "Hulk", role: "Vanguard", img: "assets/hulk.webp" },
-    { name: "Magneto", role: "Vanguard", img: "assets/magneto.webp" },
-    { name: "Peni Parker", role: "Vanguard", img: "assets/peniparker.webp" },
-    { name: "Rogue", role: "Vanguard", img: "assets/rogue.webp" },
-    { name: "The Thing", role: "Vanguard", img: "assets/thething.webp" },
-    { name: "Thor", role: "Vanguard", img: "assets/thor.webp" },
-    { name: "Venom", role: "Vanguard", img: "assets/venom.webp" },
+    { name: "Angela", role: "Vanguard", img: "assets/angela.webp", staticImg: "assets/static/angela.jpg" },
+    { name: "Captain America", role: "Vanguard", img: "assets/captainamerica.webp", staticImg: "assets/static/captainamerica.jpg" },
+    { name: "Deadpool", role: "Vanguard", img: "assets/deadpool.webp", staticImg: "assets/static/deadpool.jpg" },
+    { name: "Devil Dinosaur", role: "Vanguard", img: "assets/devildinosaur.webp", staticImg: "assets/static/devildinosaur.jpg" },
+    { name: "Doctor Strange", role: "Vanguard", img: "assets/doctorstrange.webp", staticImg: "assets/static/doctorstrange.jpg" },
+    { name: "Emma Frost", role: "Vanguard", img: "assets/emmafrost.webp", staticImg: "assets/static/emmafrost.jpg" },
+    { name: "Groot", role: "Vanguard", img: "assets/groot.webp", staticImg: "assets/static/groot.jpg" },
+    { name: "Hulk", role: "Vanguard", img: "assets/hulk.webp", staticImg: "assets/static/hulk.jpg" },
+    { name: "Magneto", role: "Vanguard", img: "assets/magneto.webp", staticImg: "assets/static/magneto.jpg" },
+    { name: "Peni Parker", role: "Vanguard", img: "assets/peniparker.webp", staticImg: "assets/static/peniparker.jpg" },
+    { name: "Rogue", role: "Vanguard", img: "assets/rogue.webp", staticImg: "assets/static/rogue.jpg" },
+    { name: "The Thing", role: "Vanguard", img: "assets/thething.webp", staticImg: "assets/static/thething.jpg" },
+    { name: "Thor", role: "Vanguard", img: "assets/thor.webp", staticImg: "assets/static/thor.jpg" },
+    { name: "Venom", role: "Vanguard", img: "assets/venom.webp", staticImg: "assets/static/venom.jpg" },
 
     // --- STRATEGISTS ---
-    { name: "Adam Warlock", role: "Strategist", img: "assets/adamwarlock.webp" },
-    { name: "Cloak & Dagger", role: "Strategist", img: "assets/cloakanddagger.webp" },
-    { name: "Deadpool", role: "Strategist", img: "assets/deadpool.webp" },
-    { name: "Gambit", role: "Strategist", img: "assets/gambit.webp" },
-    { name: "Invisible Woman", role: "Strategist", img: "assets/invisiblewoman.webp" },
-    { name: "Jeff the Land Shark", role: "Strategist", img: "assets/jeffthelandshark.webp" },
-    { name: "Loki", role: "Strategist", img: "assets/loki.webp" },
-    { name: "Luna Snow", role: "Strategist", img: "assets/lunasnow.webp" },
-    { name: "Mantis", role: "Strategist", img: "assets/mantis.webp" },
-    { name: "Rocket Raccoon", role: "Strategist", img: "assets/rocketraccoon.webp" },
-    { name: "Ultron", role: "Strategist", img: "assets/ultron.webp" },
-    { name: "White Fox", role: "Strategist", img: "assets/whitefox.webp" }
+    { name: "Adam Warlock", role: "Strategist", img: "assets/adamwarlock.webp", staticImg: "assets/static/adamwarlock.jpg" },
+    { name: "Cloak & Dagger", role: "Strategist", img: "assets/cloakanddagger.webp", staticImg: "assets/static/cloakanddagger.jpg" },
+    { name: "Deadpool", role: "Strategist", img: "assets/deadpool.webp", staticImg: "assets/static/deadpool.jpg" },
+    { name: "Gambit", role: "Strategist", img: "assets/gambit.webp", staticImg: "assets/static/gambit.jpg" },
+    { name: "Invisible Woman", role: "Strategist", img: "assets/invisiblewoman.webp", staticImg: "assets/static/invisiblewoman.jpg" },
+    { name: "Jeff the Land Shark", role: "Strategist", img: "assets/jeffthelandshark.webp", staticImg: "assets/static/jeffthelandshark.jpg" },
+    { name: "Loki", role: "Strategist", img: "assets/loki.webp", staticImg: "assets/static/loki.jpg" },
+    { name: "Luna Snow", role: "Strategist", img: "assets/lunasnow.webp", staticImg: "assets/static/lunasnow.jpg" },
+    { name: "Mantis", role: "Strategist", img: "assets/mantis.webp", staticImg: "assets/static/mantis.jpg" },
+    { name: "Rocket Raccoon", role: "Strategist", img: "assets/rocketraccoon.webp", staticImg: "assets/static/rocketraccoon.jpg" },
+    { name: "Ultron", role: "Strategist", img: "assets/ultron.webp", staticImg: "assets/static/ultron.jpg" },
+    { name: "White Fox", role: "Strategist", img: "assets/whitefox.webp", staticImg: "assets/static/whitefox.jpg" }
 ];
 
 // UI role colors
@@ -139,12 +139,14 @@ function spinRoulette() {
     let duration = 2000; 
     let intervalSpeed = 70; 
     
-    heroImg.classList.add('anim-ticking');
+    heroImg.classList.add('roulette-blur', 'anim-ticking');
 
     // Intervalo de giro (Efecto ruleta)
     const interval = setInterval(() => {
         const randomHero = poolFiltrada[Math.floor(Math.random() * poolFiltrada.length)];
-        updateUI(randomHero);
+        
+        // Pass a custom flag 'true' to show it's just spinning
+        updateUI(randomHero, true);
 
         // REPRODUCIR TICK: Reiniciamos el audio al inicio para que pueda sonar superpuesto/rápido
         if (!isMuted) {
@@ -153,12 +155,14 @@ function spinRoulette() {
         }
     }, intervalSpeed);
 
-    // Detener ruleta y dar resultado
+    // Stops the loop and drops the heavy animated file
     setTimeout(() => {
         clearInterval(interval);
         
         const finalHero = poolFiltrada[Math.floor(Math.random() * poolFiltrada.length)];
-        updateUI(finalHero);
+        
+        // Pass 'false' so it loads the animated WebP asset
+        updateUI(finalHero, false);
 
         // Play success.wav
         if (!isMuted) {
@@ -166,7 +170,7 @@ function spinRoulette() {
             successSFX.play().catch(err => console.log("Audio prevent:", err));
         }
 
-        heroImg.classList.remove('anim-ticking');
+        heroImg.classList.remove('roulette-blur', 'anim-ticking');
         isSpinning = false;
         spinBtn.disabled = false;
         spinBtn.innerText = "Spin Roulette";
@@ -179,9 +183,11 @@ function spinRoulette() {
     }, duration);
 }
 
-// Actualizar la UI
-function updateUI(hero) {
-    heroImg.src = hero.img;
+// updateUI Function
+function updateUI(hero, isSpinningPhase) {
+    // If it's spinning, use staticImg. If it's the result, drop the animated one.
+    heroImg.src = isSpinningPhase ? hero.staticImg : hero.img;
+    
     heroName.innerText = hero.name;
     heroRole.innerText = hero.role;
 
@@ -224,10 +230,15 @@ muteBtn.addEventListener('click', () => {
     }
 });
 
-// Cached images preloading
+// --- Cache Logic ---
+const imageCache = [];
 window.addEventListener('DOMContentLoaded', () => {
+    // We ONLY cache the static images. 
+    // The browser will load the animated one dynamically on demand at the end.
     heroes.forEach(heroe => {
         const img = new Image();
-        img.src = heroe.img;
+        img.src = heroe.staticImg; 
+        imageCache.push(img);
     });
+    console.log(`Cached ${heroes.length} static assets for ultra-fast spinning.`);
 });
